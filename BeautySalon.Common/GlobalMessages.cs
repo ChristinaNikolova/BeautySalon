4f1ad778acb6e62fccb37a39cbb8b4c6987756fa
christinanikolova <christina.nikolova@abv.bg>
2020-10-08 17:08:36 +0300

    implement ForgotPassword

--- BeautySalon.Common/GlobalMessages.cs
+++ BeautySalon.Common/GlobalMessages.cs
@@ -6,5 +6,10 @@
         public const string ConfirmProfileTitleMessage = "Registration Confirmation";
 
         public const string ConfirmProfileMessage = "<p>{0}, thank you for your registration at <strong>BeautySalon</strong>! Please, click <a href={1}>here</a> to confirm your email.</p>";
+
+        // Forgot Password
+        public const string ForgotPasswordTitleMessage = "Reset Password";
+
+        public const string ForgotPasswordMessage = "Please reset your password by <a href='{0}'>clicking here</a>.";
     }
 }

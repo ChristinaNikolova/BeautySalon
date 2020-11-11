21eeb3438671cba3f897a1ca057a5687f0d4f2bc
christinanikolova <christina.nikolova@abv.bg>
2020-11-11 07:56:49 +0200

    create Stylists area

---
+++ Web/BeautySalon.Web/Areas/Stylists/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿namespace BeautySalon.Web.Areas.Stylists.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class DashboardController : StylistsController
+    {
+        public IActionResult Index()
+        {
+            
+            return this.View();
+        }
+    }
+}
---
+++ Web/BeautySalon.Web/Areas/Stylists/Controllers/StylistsController.cs
@@ -0,0 +1,13 @@
+﻿namespace BeautySalon.Web.Areas.Stylists.Controllers
+{
+    using BeautySalon.Common;
+    using BeautySalon.Web.Controllers;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize(Roles = GlobalConstants.StylistRoleName)]
+    [Area("Stylists")]
+    public class StylistsController : BaseController
+    {
+    }
+}

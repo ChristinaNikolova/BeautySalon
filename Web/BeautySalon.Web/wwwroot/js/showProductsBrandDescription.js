9aa9a7d0b275527305706be5fbc73d416ee7cf62
christinanikolova <christina.nikolova@abv.bg>
2020-11-17 16:35:44 +0200

    fix small bugs refactor products

---
+++ Web/BeautySalon.Web/wwwroot/js/showProductsBrandDescription.js
@@ -0,0 +1,13 @@
+﻿function showProductsBrandDescription() {
+    document.getElementById("brand-name").addEventListener("click", showBrandDescroption);
+
+    function showBrandDescroption() {
+        let brandDescription = document.getElementById("brand-description");
+
+        if (brandDescription.style.display === "none") {
+            brandDescription.style.display = "block";
+        } else {
+            brandDescription.style.display = "none";
+        }
+    }
+}

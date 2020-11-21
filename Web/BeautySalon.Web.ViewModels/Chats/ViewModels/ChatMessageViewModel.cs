9ba4b73378fd3caf5cf3b114a91b0cc2a67b3d04
christinanikolova <christina.nikolova@abv.bg>
2020-11-21 08:04:48 +0200

    fix some UI issues add date and time to chat

--- Web/BeautySalon.Web.ViewModels/Chats/ViewModels/ChatMessageViewModel.cs
+++ Web/BeautySalon.Web.ViewModels/Chats/ViewModels/ChatMessageViewModel.cs
@@ -1,5 +1,7 @@
 ﻿namespace BeautySalon.Web.ViewModels.Chats.ViewModels
 {
+    using System;
+
     using BeautySalon.Data.Models;
     using BeautySalon.Services.Mapping;
 
@@ -14,5 +16,10 @@
         public string ReceiverUsername { get; set; }
 
         public string ReceiverPicture { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public string FormattedDate
+            => string.Format("{0:g}", this.CreatedOn);
     }
 }

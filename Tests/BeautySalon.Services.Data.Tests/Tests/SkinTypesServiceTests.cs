56ed8ec877d9c0a9e1fa87c51ce3cb0c3ad3ffb3
christinanikolova <christina.nikolova@abv.bg>
2020-11-26 18:00:48 +0200

    finish testing skin types service

--- Tests/BeautySalon.Services.Data.Tests/Tests/SkinTypesServiceTests.cs
+++ Tests/BeautySalon.Services.Data.Tests/Tests/SkinTypesServiceTests.cs
@@ -1,10 +1,82 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace BeautySalon.Services.Data.Tests.Tests
+﻿namespace BeautySalon.Services.Data.Tests.Tests
 {
-    class SkinTypesServiceTests
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using BeautySalon.Data;
+    using BeautySalon.Data.Models;
+    using BeautySalon.Data.Repositories;
+    using BeautySalon.Services.Data.SkinTypes;
+    using BeautySalon.Services.Mapping;
+    using Microsoft.EntityFrameworkCore;
+    using Xunit;
+
+    public class SkinTypesServiceTests
     {
+        public SkinTypesServiceTests()
+        {
+            new MapperInitializationProfile();
+        }
+
+        [Fact]
+        public async Task CheckGettingAllAndGettingAllAsSelectListItem()
+        {
+            ApplicationDbContext db = GetDb();
+
+            var repository = new EfDeletableEntityRepository<SkinType>(db);
+            var service = new SkinTypesService(repository);
+
+            var firstSkinType = new SkinType() { Id = Guid.NewGuid().ToString() };
+            var secondSkinType = new SkinType() { Id = Guid.NewGuid().ToString() };
+            var thirdSkinType = new SkinType() { Id = Guid.NewGuid().ToString(), Name = "Sensitive" };
+
+            await db.SkinTypes.AddAsync(firstSkinType);
+            await db.SkinTypes.AddAsync(secondSkinType);
+            await db.SkinTypes.AddAsync(thirdSkinType);
+            await db.SaveChangesAsync();
+
+            var skinTypesAsSelectListItems = await service.GetAllAsSelectListItemAsync();
+            var skinTypes = await service.GetAllAsync<TestSkinTypeModel>();
+
+            Assert.Equal(2, skinTypesAsSelectListItems.Count());
+            Assert.Equal(2, skinTypes.Count());
+        }
+
+        [Fact]
+        public async Task CheckGettingSkinTypeResult()
+        {
+            ApplicationDbContext db = GetDb();
+
+            var repository = new EfDeletableEntityRepository<SkinType>(db);
+            var service = new SkinTypesService(repository);
+
+            var firstSkinType = new SkinType() { Id = Guid.NewGuid().ToString(), Name = "Dry" };
+            var secondSkinType = new SkinType() { Id = Guid.NewGuid().ToString(), Name = "Oily" };
+            var thirdSkinType = new SkinType() { Id = Guid.NewGuid().ToString(), Name = "Sensitive" };
+
+            await db.SkinTypes.AddAsync(firstSkinType);
+            await db.SkinTypes.AddAsync(secondSkinType);
+            await db.SkinTypes.AddAsync(thirdSkinType);
+            await db.SaveChangesAsync();
+
+            var skinType = await service.GetSkinTypeResultAsync<TestSkinTypeModel>(firstSkinType.Name);
+
+            Assert.NotNull(skinType);
+            Assert.Same(firstSkinType.Id, skinType.Id);
+        }
+
+        private static ApplicationDbContext GetDb()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+            var db = new ApplicationDbContext(options);
+            return db;
+        }
+
+        public class TestSkinTypeModel : IMapFrom<SkinType>
+        {
+            public string Id { get; set; }
+        }
     }
 }

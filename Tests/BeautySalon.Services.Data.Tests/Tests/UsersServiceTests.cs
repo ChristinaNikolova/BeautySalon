26f81e68fed16ea10a2698b9c10fc9d23ada8f4d
christinanikolova <christina.nikolova@abv.bg>
2020-11-26 18:32:59 +0200

    start testing users service

--- Tests/BeautySalon.Services.Data.Tests/Tests/UsersServiceTests.cs
+++ Tests/BeautySalon.Services.Data.Tests/Tests/UsersServiceTests.cs
@@ -4,42 +4,64 @@
     using System.Threading.Tasks;
 
     using BeautySalon.Data;
+    using BeautySalon.Data.Common.Repositories;
     using BeautySalon.Data.Models;
+    using BeautySalon.Data.Models.Enums;
     using BeautySalon.Data.Repositories;
+    using BeautySalon.Services.Cloudinary;
     using BeautySalon.Services.Data.Users;
     using BeautySalon.Services.Mapping;
+    using Microsoft.AspNetCore.Http;
     using Microsoft.EntityFrameworkCore;
+    using Moq;
     using Xunit;
 
     public class UsersServiceTests
     {
+        private readonly Mock<IFormFile> mockPicture;
+
+        private readonly Mock<IRepository<SkinProblem>> skinProblemsRepository;
+        private readonly Mock<IRepository<ClientSkinProblem>> clientSkinProblemsRepository;
+        private readonly Mock<ICloudinaryService> cloudinaryService;
+
         public UsersServiceTests()
         {
             new MapperInitializationProfile();
+            this.skinProblemsRepository = new Mock<IRepository<SkinProblem>>();
+            this.clientSkinProblemsRepository = new Mock<IRepository<ClientSkinProblem>>();
+            this.cloudinaryService = new Mock<ICloudinaryService>();
+            this.mockPicture = new Mock<IFormFile>();
         }
 
         [Fact]
-        public async Task CheckGettingAllAndGettingAllAsSelectListItem()
+        public async Task CheckUpdatingUserProfile()
         {
             ApplicationDbContext db = GetDb();
 
             var repository = new EfDeletableEntityRepository<ApplicationUser>(db);
-            var service = new UsersService(repository);
+            var service = new UsersService(
+                repository,
+                this.skinProblemsRepository.Object,
+                this.clientSkinProblemsRepository.Object,
+                this.cloudinaryService.Object);
 
-            //var firstSkinType = new SkinType() { Id = Guid.NewGuid().ToString() };
-            //var secondSkinType = new SkinType() { Id = Guid.NewGuid().ToString() };
-            //var thirdSkinType = new SkinType() { Id = Guid.NewGuid().ToString(), Name = "Sensitive" };
+            var user = new ApplicationUser()
+            {
+                Id = "1",
+                FirstName = "firstName",
+                LastName = "lastName",
+                UserName = "username",
+            };
 
-            //await db.SkinTypes.AddAsync(firstSkinType);
-            //await db.SkinTypes.AddAsync(secondSkinType);
-            //await db.SkinTypes.AddAsync(thirdSkinType);
-            //await db.SaveChangesAsync();
+            await db.Users.AddAsync(user);
+            await db.SaveChangesAsync();
 
-            //var skinTypesAsSelectListItems = await service.GetAllAsSelectListItemAsync();
-            //var skinTypes = await service.GetAllAsync<TestSkinTypeModel>();
+            var picture = this.mockPicture.Object;
 
-            //Assert.Equal(2, skinTypesAsSelectListItems.Count());
-            //Assert.Equal(2, skinTypes.Count());
+            var updatedUser = await service.UpdateUserProfileAsync(user.Id, "userName", user.FirstName, "new lastName", "new address", "1223", Gender.Female.ToString(), picture);
+
+            Assert.Equal(user.FirstName, updatedUser.FirstName);
+            Assert.Equal("new lastName", updatedUser.LastName);
         }
 
         private static ApplicationDbContext GetDb()

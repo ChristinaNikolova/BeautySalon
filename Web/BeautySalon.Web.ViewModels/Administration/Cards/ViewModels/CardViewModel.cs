d6b21bc38e22b36806a16849b61c4a5ca3a5543c
christinanikolova <christina.nikolova@abv.bg>
2020-12-03 07:09:48 +0200

    finish admin cards logic refactor

--- Web/BeautySalon.Web.ViewModels/Administration/Cards/ViewModels/CardViewModel.cs
+++ Web/BeautySalon.Web.ViewModels/Administration/Cards/ViewModels/CardViewModel.cs
@@ -1,27 +1,11 @@
 ﻿namespace BeautySalon.Web.ViewModels.Administration.Cards.ViewModels
 {
-    using System;
-    using System.Globalization;
-
     using BeautySalon.Data.Models;
     using BeautySalon.Services.Mapping;
+    using BeautySalon.Web.ViewModels.Users.ViewModels;
 
-    public class CardViewModel : IMapFrom<Card>
+    public class CardViewModel : UserCardViewModel, IMapFrom<Card>
     {
-        public string ClientId { get; set; }
-
         public string ClientUsername { get; set; }
-
-        public DateTime StartDate { get; set; }
-
-        public string FormattedStartDate
-            => this.StartDate.ToString("dd MMMM yyyy", CultureInfo.CreateSpecificCulture("en"));
-
-        public DateTime EndDate { get; set; }
-
-        public string FormattedEndEnd
-           => this.EndDate.ToString("dd MMMM yyyy", CultureInfo.CreateSpecificCulture("en"));
-
-        public string TypeCardName { get; set; }
     }
 }
